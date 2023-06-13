b3ff951e26225ba6e1b67f470e6bd173abe7411b
Juan Venegas <j.venegasgutierrez@gmail.com>
2023-06-13 19:38:56 -0400

    Se desarrolla app,para viz datos de bigquery.

---
+++ app_cloud_gcp_r/server.R
@@ -0,0 +1,47 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+# Define server logic required to draw a histogram
+function(input, output, session) {
+  project_id <- "shiny-apps-385622"
+  
+  sql<-"SELECT subscriber_type,bikeid,start_time,start_station_name,end_station_name,duration_minutes from `bigquery-public-data.austin_bikeshare.bikeshare_trips` LIMIT 100"
+  
+  respuesta <- reactiveValues(data=NULL)
+  
+  observeEvent(input$boton, {
+    consulta <- bigrquery::bq_project_query(project_id, sql)
+    respuesta$datos <-bigrquery::bq_table_download(consulta,n_max = 100)
+  })
+
+  
+  output$datos_bigquery<-renderDataTable({
+    datatable(respuesta$datos)
+    },options=list(pageLength =10))
+  
+  #Aca se genera el grafico,de acuerdo a los datos extraidos en la consulta SQL.
+  #El grafico muestra en el eje x el tipo de suscriptor y en el eje y la duracion en minutos de los viajes
+  output$grafico_bigquery<-renderEcharts4r({
+    if(is.null(respuesta$datos)==TRUE){
+      
+    }else{
+      datos_graficos <- respuesta$datos %>%
+        group_by(subscriber_type) %>%
+        summarise(duration_minutes = sum(duration_minutes))
+      
+      datos_graficos |>
+        echarts4r::e_chart(subscriber_type) |>
+        echarts4r::e_bar(duration_minutes) |>
+        echarts4r::e_theme("walden")   |>
+        echarts4r::e_tooltip()
+    }
+    
+  })
+}
---
+++ app_cloud_gcp_r/ui.R
@@ -0,0 +1,68 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(htmlwidgets)
+library(dplyr)
+library(DT)
+library(echarts4r)
+#library(utils)
+library(bs4Dash)
+#library(reactable)
+#library(MASS)
+#library(config)
+#library(htmltools)
+#library(lubridate)
+#library(shinyWidgets)
+#library(shinycssloaders)
+#library(reticulate)
+library(shiny.i18n)
+library(bigrquery)
+library(echarts4r)
+source("traductor.R")
+
+bigrquery::bq_auth(path ="shiny-apps-385622-08e5b9820326.json")
+
+ui <-  dashboardPage(
+  
+  dashboardHeader(title = i18n$t("App_cloud_gcp_r")),
+  dashboardSidebar(side = "top", visible = FALSE, status = "teal",
+                   sidebarMenu(
+                     id = "sidebar",
+                     menuItem(i18n$t("Inicio"),tabName = "menu1",
+                              icon=icon("laptop-medical"),
+                              selected = TRUE),
+                     
+                     bs4Dash::menuItem(i18n$t("Analisis de datos Bigquery"),tabName="menu2",
+                                       icon=icon("check-square")),
+                     selectInput(inputId = "idioma", label = "Elige idioma", choices = c("Spanish" = "es","English" = "en"))
+                     )),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "menu1",
+              
+              fluidRow(width=12,imageOutput("myImage")),
+              fluidRow(width=12,
+                       bs4Dash::infoBox(width = 12,title = shiny::h3(i18n$t("Analisis de datos Bigquery"), style = 'font-size:30px'),subtitle=i18n$t("Este es un modulo para analisis de datos del servicio Bigquery"), 
+                                        icon=shiny::icon("arrow-pointer"), tabName = "menu2",color = "lightblue",fill=FALSE, iconElevation = 2,elevation = 2)),
+              
+      ), 
+
+      tabItem(tabName = "menu2",
+              fluidRow(actionButton(inputId = "boton",label =  "Descarga")),
+              fluidRow(width=12,box(dataTableOutput("datos_bigquery",width = "100%"),
+                                   width = 6,status = "lightblue",headerBorder = FALSE,collapsible = FALSE,closable = FALSE,elevation = 2),
+                               box(title = i18n$t("Datos de set de datos 1"),echarts4rOutput("grafico_bigquery",width = "100%"),
+                                   width = 6,status = "lightblue",headerBorder = FALSE,collapsible = FALSE,closable = FALSE,elevation = 2))
+                                
+              )
+    )
+    
+  ))
+
